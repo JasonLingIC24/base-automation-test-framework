213347f78914524d6700405cf5d3955d01ce4716
Jason Ling <jason.ling@ic24.nhs.uk>
2025-11-07 12:50:34 +0000

    update config to use env file add regex pattern

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const { defineConfig } = require('cypress');
-const { initPlugins } = require('./cypress/support/config-support');
+require("dotenv").config();
+const { defineConfig } = require("cypress");
+const { initPlugins } = require("./cypress/support/config-support");
 
 module.exports = defineConfig({
   defaultCommandTimeout: 10000,
@@ -13,12 +13,12 @@ module.exports = defineConfig({
       // implement node event listeners here
       initPlugins(on, [
         (on) => {
-          on('before:browser:launch', (launchOptions, browser = {}) => {
+          on("before:browser:launch", (launchOptions, browser = {}) => {
             // Disable shared memory when run since most CI environment do not support that
-            if (browser.name === 'chrome' || browser.name === 'chromium') {
-              launchOptions.args.push('--disable-dev-shm-usage');
-            } else if (browser.name === 'electron') {
-              launchOptions.args['disable-dev-shm-usage'] = true;
+            if (browser.name === "chrome" || browser.name === "chromium") {
+              launchOptions.args.push("--disable-dev-shm-usage");
+            } else if (browser.name === "electron") {
+              launchOptions.args["disable-dev-shm-usage"] = true;
             }
           });
         },
@@ -27,15 +27,15 @@ module.exports = defineConfig({
       return config;
     },
 
-    baseUrl: 'http://localhost:3000',
-    specPattern: 'cypress/tests/**/*.spec.{js,jsx,ts,tsx}',
-    excludeSpecPattern: 'cypress/tests/**/*.spec.skip.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.ts',
+    baseUrl: process.env.PATHWAYS_TEST_URL,
+    specPattern: "cypress/tests/**/*.spec.{js,jsx,ts,tsx}",
+    excludeSpecPattern: "cypress/tests/**/*.spec.skip.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.ts",
     viewportWidth: 1280,
     viewportHeight: 1000,
     chromeWebSecurity: false,
-    screenshotsFolder: 'cypress/artifacts/screenshots',
-    videosFolder: 'cypress/artifacts/videos',
+    screenshotsFolder: "cypress/artifacts/screenshots",
+    videosFolder: "cypress/artifacts/videos",
     trashAssetsBeforeRuns: true,
   },
 
---
+++ cypress/support/dataObjects/regex.patterns.data.objects.ts
@@ -0,0 +1,11 @@
+export const domain =
+  "^http(s)?://(www\\.)?(int\\.)?pathways\\.sehnp\\.nhs\\.uk";
+
+// https://int.pathways.sehnp.nhs.uk
+
+export const uuid =
+  "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
+
+export const emailAddress = "[a-z0-9._%+-]+@(?:[a-z0-9-]+.)+[a-z]{2,}";
+
+export default {};
